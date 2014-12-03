37504a124d7ceb5fb4ff66348dc567deace6aa48
pindziukou <pahanez@gmail.com>
2014-12-03 20:44:19 +0100

    run

---
+++ presenter/src/androidTest/java/famtask/pahanez/com/familytask/data/MockProviderTest.java
@@ -0,0 +1,20 @@
+package famtask.pahanez.com.familytask.data;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+/**
+ * Created by pindziukou on 03.12.14.
+ */
+public class MockProviderTest extends TestCase{
+
+
+    @SmallTest
+    public void testSomeStuff(){
+        System.out.println("start");
+        Assert.assertEquals("1","1");
+    }
+}
